a54a302040dfa497ee1baf5ca05b43ac1d5a8a85
mannicchiarico <mannicchiarico@uc.cl>
2025-04-18 17:06:41 -0400

    Modifiqué controller devise regitrations para considerar los nuevos atributos, y restringí a que no se repitan los nombres de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_18_202944) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_18_210234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_18_202944) do
     t.string "description", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "blog_participations", "blogs"
