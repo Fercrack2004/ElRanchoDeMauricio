b85e548889419aa9d1d649d8acb5e4762645495b
Fercrack2004 <fernando.gajardo@uc.cl>
2025-04-18 18:13:25 -0400

    Crear blog e informacion index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
-                   path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  get 'render/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'render/index'
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "render#index"
+  # Rutas para blogs
+  resources :blogs, only: [:new, :create, :index, :show]
+  resources :information
 
+  root "render#index"
 end
