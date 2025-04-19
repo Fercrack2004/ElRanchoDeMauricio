6a05b216cc86fa539f9a65cf6b1155cffe95f3e3
mannicchiarico <mannicchiarico@uc.cl>
2025-04-19 13:56:52 -0400

    Crar views, tutas y módulos de las solicitudes, añadir botones pertinentes

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :requestable, polymorphic: true
-  enum state: { Pendiente: 0, Aceptado: 1, Rechazado: 2 }
+  enum state: { pending: 0, accepted: 1, rejected: 2 }
 
 # Validaciones
   validates :requestable, presence: true
