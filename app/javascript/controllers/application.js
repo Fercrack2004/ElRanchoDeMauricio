9e2f2ffae8ca51702ecadaccaec7a9735af17f61
Fercrack2004 <fernando.gajardo@uc.cl>
2025-04-19 11:20:10 -0400

    turbo false jv

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,5 +5,8 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+// pal turbo
+import "@hotwired/turbo-rails"
+Turbo.session.drive = false
 
 export { application }
