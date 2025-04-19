b07aca9690b5463dcf0a39b9cda9138142d61810
mannicchiarico <mannicchiarico@uc.cl>
2025-04-19 12:50:15 -0400

    Rubocop, controller y views index, show, new de requests, poner botones de ver y dejar review dentro del blog

--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -1,8 +1,8 @@
 class RenderController < ApplicationController
   def index
     @blogs = Blog.includes(:blog_participations, :users)
-                .order(created_at: :desc)
-                .limit(3)
+                 .order(created_at: :desc)
+                 .limit(3)
   rescue => e
     @blogs = []
     logger.error "Error al cargar blogs: #{e.message}"
