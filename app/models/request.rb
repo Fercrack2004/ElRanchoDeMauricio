0385d37c7499c48c32d6a1d4414fb96afc84e8f2
faquefer <felipe.quezada@uc.cl>
2025-04-19 23:18:52 -0400

    se verifica que un usuario tiene que ser editor para participar en los chats de los blogs

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,8 @@ class Request < ApplicationRecord
   belongs_to :requestable, polymorphic: true
   enum state: { pending: 0, accepted: 1, rejected: 2 }
 
+
+    
 # Validaciones
   validates :requestable, presence: true
 
