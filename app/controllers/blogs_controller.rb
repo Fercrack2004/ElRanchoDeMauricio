56f18c3522430d04f9cd5f6f59dc6d97ab234487
Fercrack2004 <fernando.gajardo@uc.cl>
2025-04-20 19:24:02 -0400

    Barra de busqueda funcional en la pagina principal

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,45 +1,50 @@
 class BlogsController < ApplicationController
-
-  def index
-    @blog = Blog.all
-  end
-
-  def show
-    @blog = Blog.find(params[:id])
-  end
-
-  def new
-    @blog = Blog.new
-  end
-    
-  def create
-    @blog = Blog.new(blog_params)
+    def index
+      @search_term = params[:search]
+      @blog = if @search_term.present?
+                 Blog.where("title LIKE ? OR description LIKE ?", 
+                           "%#{@search_term}%", "%#{@search_term}%")
+               else
+                 Blog.all
+               end.order(created_at: :desc)
+    end
+  
+    def show
+      @blog = Blog.find(params[:id])
+    end
+  
+    def new
+      @blog = Blog.new
+    end
+      
+    def create
+      @blog = Blog.new(blog_params)
       if @blog.save
         # Asociar al usuario como autor
         @blog.blog_participations.create(user: current_user, contribution: :autor)
         # Crear el chatroom correspondiente
         @blog.create_chat_room(name: "Chat de editores: \"#{@blog.title}\"")
-          redirect_to @blog, notice: "Blog creado con éxito!"
+        redirect_to @blog, notice: "Blog creado con éxito!"
       else
         render :new, status: :unprocessable_entity
       end
-  end
+    end
+  
+    def edit
+      @blog = Blog.find(params[:id])
+    end
 
-  def edit
-    @blog = Blog.find(params[:id])
-  end
-
-  def update
-    @blog = Blog.find(params[:id])
-    if @blog.update(blog_params)
-      redirect_to @blog
+    def update
+        @blog = Blog.find(params[:id])
+        if @blog.update(blog_params)
+          @blog.chat_room.update(name: "Chat de editores: \"#{@blog.title}\"")
+          redirect_to @blog
+        end
+      end
+  
+    private
+  
+    def blog_params
+      params.require(:blog).permit(:title, :public_type, :description)
     end
   end
-
-  private
-
-  def blog_params
-    params.require(:blog).permit(:title, :public_type, :description)
-  end
-
-end
--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -1,9 +1,35 @@
 class RenderController < ApplicationController
   def index
-    @blogs = Blog.order(created_at: :desc).limit(3)
-    @informations = Information.order(created_at: :desc).limit(3)
+    @search_term = params[:search]
+    
+    if @search_term.present?
+      # Modo búsqueda like searching fr
+      @blogs = search_blogs(@search_term)
+      @informations = search_informations(@search_term)
+      @showing_search_results = true
+    else
+      # Modo normal like normie asf
+      @blogs = Blog.order(created_at: :desc).limit(3)
+      @informations = Information.order(created_at: :desc).limit(3)
+      @showing_search_results = false
+    end
   rescue ActiveRecord::RecordNotFound
     @blogs = []
     @informations = []
+    @showing_search_results = false
+  end
+
+  private
+
+  def search_blogs(term)
+    Blog.where("title LIKE ? OR description LIKE ?", 
+              "%#{term}%", "%#{term}%")
+       .order(created_at: :desc)
+  end
+
+  def search_informations(term)
+    Information.where("title LIKE ? OR description LIKE ?", 
+                     "%#{term}%", "%#{term}%")
+              .order(created_at: :desc)
   end
 end
