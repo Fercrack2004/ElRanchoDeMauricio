54e85fd4437ffa85a6603e732b7d86cb0f8f7499
Fercrack2004 <fernando.gajardo@uc.cl>
2025-05-16 19:50:13 -0400

    frontend actualizado, tailwind implementado

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,8 +5,5 @@ const application = Application.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-// pal turbo
-import "@hotwired/turbo-rails"
-Turbo.session.drive = false
 
 export { application }
