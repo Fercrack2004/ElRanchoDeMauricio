c130a81f9d7cc11d520009dffa89334b6e3d3b1e
mannicchiarico <mannicchiarico@uc.cl>
2025-05-16 01:10:52 -0400

    Debuggeando render 4

--- db/migrate/20250418164023_create_information_participations.rb
+++ db/migrate/20250418164023_create_information_participations.rb
@@ -2,10 +2,14 @@ class CreateInformationParticipations < ActiveRecord::Migration[7.1]
   def change
     create_table :information_participations do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :information, null: false, foreign_key: true
+
+      t.bigint :information_id, null: false
       t.integer :contribution
 
       t.timestamps
     end
+
+    add_index :information_participations, :information_id
+    add_foreign_key :information_participations, :information_pages, column: :information_id
   end
 end
