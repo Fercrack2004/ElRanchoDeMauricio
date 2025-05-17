66c5c783d9335971b1e35aa23dea98aec511bd20
mannicchiarico <mannicchiarico@uc.cl>
2025-05-16 20:16:18 -0400

    Arreglar cosas de tailwind, agregar cook_time al formulario y controller, restringir acceso a los chatrooms y a quien puede mandar mensaje en los controllers respectivos

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,19 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_chat_room
+  before_action :authorize_user!
 
   def create
     @message = @chat_room.messages.create(message_params.merge(user: current_user))
-      if @message.persisted?
-        redirect_to [@chatable, @chat_room]
-      else
-        flash.now[:alert] = "Message could not be sent. Please try again."
-          redirect_to [@chatable, @chat_room]
-      end
+    if @message.persisted?
+      redirect_to [@chatable, @chat_room]
+    else
+      flash.now[:alert] = "Mensaje no pudo ser enviado. Por favor inténtalo de nuevo."
+      redirect_to [@chatable, @chat_room]
+    end
   end
 
-    private
+  private
 
   def set_chat_room
     if params[:blog_id]
@@ -20,11 +21,29 @@ class MessagesController < ApplicationController
     elsif params[:information_id]
       @chatable = Information.find_by(id: params[:information_id])
     end
-      @chat_room = @chatable.chat_room
+    @chat_room = @chatable&.chat_room
+  end
+
+  def authorize_user!
+    return if @chatable.blank? || @chat_room.blank?
+
+    participation = find_participation
+
+    unless participation&.autor_contribution? || participation&.editor_contribution? ||
+           current_user.admin?
+      redirect_to root_path, alert: "No tienes permiso para enviar mensajes en este chat."
+    end
+  end
+
+  def find_participation
+    if @chatable.is_a?(Blog)
+      @chatable.blog_participations.find_by(user: current_user)
+    elsif @chatable.is_a?(Information)
+      @chatable.information_participations.find_by(user: current_user)
+    end
   end
 
   def message_params
-    params.require(:message).permit(:user, :content)
+    params.require(:message).permit(:content)
   end
-
 end
