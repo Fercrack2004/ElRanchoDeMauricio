1bbcac044bf77079976decc7cdbeb5b2ab35b1ab
mannicchiarico <mannicchiarico@uc.cl>
2025-06-07 14:56:56 -0400

    quitar linea en render-build pq ya se corrió

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,8 +9,6 @@ bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-bundle exec rails runner "Subscriber.delete_all"
-
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
